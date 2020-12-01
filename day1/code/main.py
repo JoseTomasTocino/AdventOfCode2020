43e3b22557d55179e83968f97b83310ab9f118ce
José Tomás Tocino <theom3ga@gmail.com>
2020-12-01 09:37:07 +0100

    Initial code commit

---
+++ day1/code/main.py
@@ -0,0 +1,24 @@
+import functools
+import logging
+import operator
+
+logger = logging.getLogger(__name__)
+
+
+def get_number_group(numbers, num_components=1):
+    for i, n in enumerate(numbers):
+        if num_components == 1:
+            yield [n]
+        else:
+            for subgroup in get_number_group(numbers[i + 1:], num_components - 1):
+                yield [n] + subgroup
+
+
+def process_input(s, num_components=2):
+    numbers = [int(x.strip()) for x in s.split("\n")]
+
+    numbers.sort(reverse=True)
+
+    for group in get_number_group(numbers, num_components):
+        if sum(group) == 2020:
+            return functools.reduce(operator.mul, group)
---
+++ day1/test/test_main.py
@@ -0,0 +1,58 @@
+import logging
+
+from day1.code.main import process_input, get_number_group
+
+logger = logging.getLogger(__name__)
+
+
+def test_case_1():
+    input_str = """1721
+979
+366
+299
+675
+1456"""
+
+    assert process_input(input_str) == 514579
+
+
+def test_case_2():
+    input_str = """1721
+979
+366
+299
+675
+1456"""
+
+    assert process_input(input_str, num_components=3) == 241861950
+
+
+def test_input():
+    with open('input', 'r') as f:
+        input_str = f.read()
+        output = process_input(input_str)
+        logger.info(f"Output: {output}")
+
+        assert output == 388075
+
+
+def test_input_with_three_components():
+    with open('input', 'r') as f:
+        input_str = f.read()
+        output = process_input(input_str, num_components=3)
+        logger.info(f"Output: {output}")
+
+        assert output == 293450526
+
+
+def test_get_number_group():
+    numbers_in = [1, 2, 3, 4]
+
+    numbers_out = list(get_number_group(numbers_in, num_components=1))
+    assert numbers_out == [[1], [2], [3], [4]]
+
+    numbers_out = list(get_number_group(numbers_in, num_components=2))
+    assert numbers_out == [[1, 2], [1, 3], [1, 4], [2, 3], [2, 4], [3, 4]]
+
+    numbers_out = list(get_number_group(numbers_in, num_components=3))
+    assert numbers_out == [[1, 2, 3], [1, 2, 4], [1, 3, 4], [2, 3, 4]]
