c6f6d63acd959f86c162c2f52f46bcb858efffd7
José Tomás Tocino <josetomas.tocino@gmail.com>
2020-12-02 11:02:49 +0100

    Add day2 solutions

---
+++ day2/code/main.py
@@ -0,0 +1,51 @@
+import re
+import logging
+from collections import Counter
+
+logger = logging.getLogger(__name__)
+
+
+def get_policy_components(policy):
+    policy_match = re.match(r'(\d+)-(\d+)\s+([a-zA-Z]+)', policy)
+        
+    leftr = int(policy_match.group(1))
+    rightr = int(policy_match.group(2))
+    char = policy_match.group(3).strip()
+
+    return (leftr, rightr, char)
+
+
+def check_password_list(l):    
+    correct_passwords = 0
+
+    for policy, password in [x.split(": ") for x in l.split("\n")]:
+        logger.info(f"Policy: {policy}, password: {password}")
+
+        policy_min, policy_max, policy_char = get_policy_components(policy)
+        
+        password_char_count = password.count(policy_char)
+
+        if policy_min <= password_char_count <= policy_max:
+            correct_passwords += 1
+
+    return correct_passwords
+
+
+def check_password_list_with_new_policy(l):    
+    correct_passwords = 0
+
+    for policy, password in [x.split(": ") for x in l.split("\n")]:
+        logger.info(f"Policy: {policy}, password: '{password}'")
+
+        first_position, second_position, policy_char = get_policy_components(policy)
+       
+        char_in_first = password[first_position - 1] == policy_char
+        char_in_second = password[second_position - 1] == policy_char
+
+        if char_in_first != char_in_second:
+            correct_passwords += 1
+
+    return correct_passwords    
+        
+
+        
---
+++ day2/test/test_main.py
@@ -0,0 +1,35 @@
+from day2.code.main import check_password_list, check_password_list_with_new_policy
+import os.path
+import logging
+
+logger = logging.getLogger(__name__)
+
+local_path = os.path.abspath(os.path.dirname(__file__))
+
+def test_sample_input():
+    sample_input = """1-3 a: abcde
+1-3 b: cdefg
+2-9 c: ccccccccc"""
+
+    assert check_password_list(sample_input) == 2
+
+def test_big_input():
+    with open(os.path.join(local_path, "input"), 'r') as f:
+        correct_passwords = check_password_list(f.read())
+        logger.info(f"Correct passwords: {correct_passwords}")
+        assert correct_passwords == 603
+
+
+def test_sample_input_with_new_policy():
+    sample_input = """1-3 a: abcde
+1-3 b: cdefg
+2-9 c: ccccccccc"""
+
+    assert check_password_list_with_new_policy(sample_input) == 1
+
+
+def test_big_input_with_new_policy():
+    with open(os.path.join(local_path, "input"), 'r') as f:
+        correct_passwords = check_password_list_with_new_policy(f.read())
+        logger.info(f"Correct passwords: {correct_passwords}")
+        assert correct_passwords == 404        
