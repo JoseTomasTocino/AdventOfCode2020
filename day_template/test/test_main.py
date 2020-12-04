eab58fba25cd883281a4cc38647b7f24705d6fba
José Tomás Tocino <theom3ga@gmail.com>
2020-12-04 08:07:31 +0100

    Update template files

--- day_template/test/test_main.py
+++ day_template/test/test_main.py
@@ -1,3 +1,16 @@
 import logging
+import os.path
 
 logger = logging.getLogger(__name__)
+local_path = os.path.abspath(os.path.dirname(__file__))
+
+sample_input = None
+
+
+def test_sample_input():
+    pass
+
+
+def test_big_input():
+    with open(os.path.join(local_path, "input"), 'r') as f:
+        f.read()
