c3768029aad908feb7c1be02d6c607c14f6c38a1
José Tomás Tocino <theom3ga@gmail.com>
2020-12-06 07:16:05 +0100

    Create day6, second part

--- day6/code/main.py
+++ day6/code/main.py
@@ -4,8 +4,9 @@ import re
 logger = logging.getLogger(__name__)
 
 
-def count_questions(in_str):
-    # Isolate groups
-    yes_count = sum([len(set(x.replace("\n", ""))) for x in in_str.split("\n\n")])
+def count_questions_anyone(in_str):
+    return sum([len(set(x.replace("\n", ""))) for x in in_str.split("\n\n")])
 
-    return yes_count
+
+def count_questions_everyone(in_str):
+    return sum([len(set.intersection(*[set(x) for x in group.split("\n")])) for group in in_str.split("\n\n")])
--- day6/test/test_main.py
+++ day6/test/test_main.py
@@ -1,7 +1,7 @@
 import logging
 import os.path
 
-from day6.code.main import count_questions
+from day6.code.main import count_questions_anyone, count_questions_everyone
 
 logger = logging.getLogger(__name__)
 local_path = os.path.abspath(os.path.dirname(__file__))
@@ -24,13 +24,15 @@ b"""
 
 
 def test_sample_input():
-    assert count_questions(sample_input) == 11
+    assert count_questions_anyone(sample_input) == 11
+    assert count_questions_everyone(sample_input) == 6
 
 
 def test_big_input():
     with open(os.path.join(local_path, "input"), 'r') as f:
-        highest_bp_id = count_questions(f.read())
-        assert highest_bp_id == 6387
+        content = f.read()
+        assert count_questions_anyone(content) == 6387
+        assert count_questions_everyone(content) == 3039
 #
 #
 # def test_find_my_seat():
