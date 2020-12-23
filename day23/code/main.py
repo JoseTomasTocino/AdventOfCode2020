bc6cd9a518267d815e840d2c5e926ae42f97ceee
José Tomás Tocino <theom3ga@gmail.com>
2020-12-23 09:00:10 +0100

    Improved solution for day 23, part 1

--- day23/code/main.py
+++ day23/code/main.py
@@ -1,111 +1,188 @@
+import dataclasses
 import logging
+from dataclasses import dataclass
 
 logger = logging.getLogger(__name__)
 
 
-def displace_left(the_list, positions):
-    positions = positions % len(the_list)
+# def displace_left(the_list, positions):
+#     positions = positions % len(the_list)
+#
+#     for i in range(positions):
+#         the_list = the_list[1:] + [the_list[0]]
+#
+#     return the_list
+#
+#
+# def displace_right(the_list, positions):
+#     positions = positions % len(the_list)
+#
+#     for i in range(positions):
+#         the_list = [the_list[-1]] + the_list[:-1]
+#
+#     return the_list
 
-    for i in range(positions):
-        the_list = the_list[1:] + [the_list[0]]
 
-    return the_list
+@dataclass
+class Node:
+    first: int
+    last: int
+    next: 'Node' = dataclasses.field(default=None, repr=False)
+
+    def is_single(self):
+        return self.first == self.last
+
+    def as_short_string(self):
+        if self.is_single():
+            return str(self.first)
+
+        else:
+            return f'{self.first}-{self.last}'
+
+    def as_long_string(self):
+        if self.is_single():
+            return str(self.first)
+
+        else:
+            return ', '.join(str(x) for x in range(self.first, self.last + 1))
+
+    def get(self, amount=3):
+        the_next = []
+        curr = self
+
+        while len(the_next) < amount:
+            curr.split()
+            the_next.append(curr)
+            curr = curr.next
+
+        return the_next
+
+    def get_next(self, amount=3):
+        if not self.is_single():
+            self.split()
+
+        return self.next.get(amount=amount)
+
+    def split(self):
+        if self.is_single():
+            return
+
+        right = Node(first=self.first + 1, last=self.last, next=self.next)
+        self.last = self.first
+        self.next = right
+
+        return self, right
+
+    def chain_str(self, initial=None, expand=False):
+        if initial == self:
+            return ''
+
+        else:
+            if initial is None:
+                initial = self
+
+            s = self.as_long_string() if expand else self.as_short_string()
+
+            if self.next is not None and self.next is not initial:
+                s = s + ', ' + self.next.chain_str(initial=initial, expand=expand)
+
+            return s
+
+    def find(self, value):
+        if self.first <= value <= self.last:
+            return self
+        else:
+            return self.next.find(value)
 
 
-def displace_right(the_list, positions):
-    positions = positions % len(the_list)
+def nodes_from_list(cups):
+    i = 0
+    nodes = []
+    last_node = None
 
-    for i in range(positions):
-        the_list = [the_list[-1]] + the_list[:-1]
+    # Create the nodes, using a single node for consecutive cups
+    while i < len(cups):
+        if last_node is None:
+            last_node = Node(first=cups[i], last=cups[i])
+            nodes.append(last_node)
 
-    return the_list
+        else:
+            if cups[i] == cups[i - 1] + 1:
+                last_node.last += 1
+
+            else:
+                last_node = Node(first=cups[i], last=cups[i])
+                nodes[-1].next = last_node
+                nodes.append(last_node)
+
+        i += 1
+
+    # Full circle!
+    nodes[-1].next = nodes[0]
+
+    return nodes
 
 
 def play_cups_game(inp, max_moves=100, use_extended_rules=False):
     cups = [int(x) for x in inp.strip()]
-    current_move = 1
-    current_cup_index = 0
+    max_cup = max(cups)
 
-    if use_extended_rules:
-        cups = cups + list(range(max(cups) + 1, max(cups) + 1 + 1000000 - len(cups)))
+    logger.info(f"Cups: {cups}")
+
+    nodes = nodes_from_list(cups)
+
+    current = nodes[0]
+
+    current_move = 1
+    #
+    # if use_extended_rules:
+    #     cups = cups + list(range(max(cups) + 1, max(cups) + 1 + 1000000 - len(cups)))
 
     while current_move <= max_moves:
         logger.info(f"-- move {current_move} --")
-        logger.info(f"current cup index: {current_cup_index}")
-        logger.info(
-            f"cups: {' '.join([str(c) if i != current_cup_index else '(' + str(c) + ')' for i, c in enumerate(cups)])}")
+        logger.info(f"Nodes: {nodes[0].chain_str(expand=True)}")
 
-        current_cup_label = cups[current_cup_index]
+        # The crab picks up the three cups that are immediately clockwise of the current cup.
+        picked_up = current.get_next(amount=3)
+        picked_up_values = [x.first for x in picked_up]
 
-        # The crab picks up the three cups that are immediately clockwise of the current cup. They are removed from
-        # the circle; cup spacing is adjusted as necessary to maintain the circle.
+        logger.info(f"pick up: {picked_up_values}")
 
-        pick_up_indices = [(current_cup_index + i) % len(cups) for i in range(1, 4)]
-        picked_up = [cups[x] for x in pick_up_indices]
+        # They are removed from the circle; cup spacing is adjusted as necessary to maintain the circle.
+        current.next = picked_up[-1].next
+        picked_up[-1].next = None
 
-        for picked in picked_up:
-            cups.remove(picked)
+        # The crab selects a destination cup: the cup with a label equal to the current cup's label minus one.
+        destination = current.first - 1
 
-        logger.info(f"pick up: {', '.join(str(x) for x in picked_up)}")
-        logger.info(
-            f"after pickup: {' '.join([str(c) if i != current_cup_index else '(' + str(c) + ')' for i, c in enumerate(cups)])}")
-
-        # The crab selects a destination cup: the cup with a label equal to the current cup's label minus one. If
-        # this would select one of the cups that was just picked up, the crab will keep subtracting one until it
+        # If this would select one of the cups that was just picked up, the crab will keep subtracting one until it
         # finds a cup that wasn't just picked up. If at any point in this process the value goes below the lowest
         # value on any cup's label, it wraps around to the highest value on any cup's label instead.
 
-        destination = current_cup_label - 1
         while True:
-            if destination in cups:
+            if destination == 0:
+                destination = max_cup
+
+            if destination not in picked_up_values:
                 break
 
-            elif destination in picked_up:
-                destination -= 1
+            destination -= 1
 
-            if destination == 0:
-                destination = max(cups)
-
-        destination_index = cups.index(destination)
         logger.info(f"destination: {destination}")
-        # logger.info(f"dest. index: {destination_index}")
+        destination = current.find(destination)
 
         # The crab places the cups it just picked up so that they are immediately clockwise of the destination cup.
         # They keep the same order as when they were picked up.
 
-        cups = cups[0:destination_index + 1] + picked_up + cups[destination_index + 1:]
-        logger.info(
-            f"after insert: {' '.join([str(c) if i != current_cup_index else '(' + str(c) + ')' for i, c in enumerate(cups)])}")
-
-        # Displace the list so that the current cup stays at the same index
-        displacement = cups.index(current_cup_label) - current_cup_index
-
-        if displacement > 0:
-            cups = displace_left(cups, displacement)
-        else:
-            cups = displace_right(cups, displacement)
-
-        # # If the destination is left of the current cup, modify the cups list so that the current index stays correct
-        # if destination_index < current_cup_index:
-        #     old_cups = cups[:]
-        #     cups = displace_left(cups, 3)
-        #     logger.info(f"Displacing left:")
-        #     logger.info(f"from {old_cups}")
-        #     logger.info(f"to   {cups}")
-        #
-        # # If elements were picked from the left side of the current cup index, displace right
-        # cups = displace_right(cups, picked_up_from_the_left)
-
-        logger.info(
-            f"after displace: {' '.join([str(c) if i != current_cup_index else '(' + str(c) + ')' for i, c in enumerate(cups)])}")
+        picked_up[-1].next = destination.next
+        destination.next = picked_up[0]
 
         # The crab selects a new current cup: the cup which is immediately clockwise of the current cup.
-        current_cup_index = (current_cup_index + 1) % len(cups)
+        current = current.next
 
         logger.info("")
         current_move += 1
 
-    logger.info(f"Cups: {cups}")
+    node_one = nodes[0].find(1)
 
-    one_at = cups.index(1)
-    return ''.join(str(x) for x in (cups[one_at + 1:] + cups[0:one_at]))
+    return ''.join(node_one.chain_str(expand=True)[3:].split(", "))
--- day23/test/test_main.py
+++ day23/test/test_main.py
@@ -1,7 +1,7 @@
 import logging
 import os.path
 
-from day23.code.main import play_cups_game, displace_left, displace_right
+from day23.code.main import play_cups_game
 
 logger = logging.getLogger(__name__)
 local_path = os.path.abspath(os.path.dirname(__file__))
@@ -9,14 +9,14 @@ local_path = os.path.abspath(os.path.dirname(__file__))
 sample_input = None
 
 
-def test_displacement(caplog):
-    caplog.set_level(logging.INFO)
-
-    assert displace_left([1, 2, 3, 4, 5, 6], 2) == [3, 4, 5, 6, 1, 2]
-    assert displace_left([1, 2, 3, 4, 5, 6], 2 * 10) == [3, 4, 5, 6, 1, 2]
-
-    assert displace_right([1, 2, 3, 4, 5, 6], 2) == [5, 6, 1, 2, 3, 4]
-    assert displace_right([1, 2, 3, 4, 5, 6], 2 * 10) == [5, 6, 1, 2, 3, 4]
+# def test_displacement(caplog):
+#     caplog.set_level(logging.INFO)
+#
+#     assert displace_left([1, 2, 3, 4, 5, 6], 2) == [3, 4, 5, 6, 1, 2]
+#     assert displace_left([1, 2, 3, 4, 5, 6], 2 * 10) == [3, 4, 5, 6, 1, 2]
+#
+#     assert displace_right([1, 2, 3, 4, 5, 6], 2) == [5, 6, 1, 2, 3, 4]
+#     assert displace_right([1, 2, 3, 4, 5, 6], 2 * 10) == [5, 6, 1, 2, 3, 4]
 
 
 def test_sample_input(caplog):
@@ -29,6 +29,6 @@ def test_sample_input(caplog):
 
 
 def test_big_input(caplog):
-    caplog.set_level(logging.INFO)
+    # caplog.set_level(logging.INFO)
 
     assert play_cups_game("974618352") == "75893264"
